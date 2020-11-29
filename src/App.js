061ca58d85f8624fc1bffa815a973f0c5d2e0034
Alexander Almström <alexander.almstrom@panagora.se>
2020-11-29 02:13:26 +0100

    Simplify load posts

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
   const [{ posts }, dispatch] = useReducer(postReducer, { posts: [] });
 
   const loadPosts = async () => {
-    const payload = await getPosts();
-
-    dispatch({ type: "GET_POSTS", payload });
+    dispatch({ type: "GET_POSTS", payload: await getPosts() });
   };
 
   useEffect(() => {
